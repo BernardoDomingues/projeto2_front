7213aef823dbe9ea25ece8aa00717f21157e217b
Bernardo Domingues <bernardoopd@hotmail.com>
2021-11-23 14:32:51 -0300

    Adiciona Rotas e cria primeiras págias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-const App = () => {
-  return <div>Teste</div>;
-};
+import React from "react";
+import Home from "./containers/Home";
+import About from "./containers/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-export default App;
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/sobre">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
